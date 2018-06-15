d548ff9024f53440539a53d6d88dfeac07c89f8f
Marcel <linusdufe@gmail.com>
2018-06-15 12:18:47 +0200

    g

--- src/cocherecuperacionad/CocheRecuperacionAD.java
+++ src/cocherecuperacionad/CocheRecuperacionAD.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -16,8 +17,9 @@ import java.util.ArrayList;
  *
  * @author linusdufol
  */
-public class CocheRecuperacionAD {
 
+public class CocheRecuperacionAD {
+private static Coche coche;
     /**
      * @param args the command line arguments
      */
@@ -27,98 +29,98 @@ public class CocheRecuperacionAD {
         File email = null;
         FileReader fr = null;
         
-        ArrayList<String> arrayRenault = new ArrayList<>();
-        ArrayList<String> arraySeat = new ArrayList<>();
+        ArrayList<String> arrayPalabras = new ArrayList<>();
+        
         
         try {
+            
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             //path archivos Marcel
             String pathM1 = "/Users/linusdufol/Documents/workspace/CocheRecuperacionAD/src/cocherecuperacionad/renault.txt";
-            String pathM2 = "/Users/linusdufol/Documents/workspace/CocheRecuperacionAD/src/cocherecuperacionad/seat.txt";
+            //String pathM2 = "/Users/linusdufol/Documents/workspace/CocheRecuperacionAD/src/cocherecuperacionad/seat.txt";
             //Path archivos Renzo
-            String pathR1 = "PON AQUI TU PATH DEL ARCHIVO RENAULT.TXT";
-            String pathR2 = "PON AQUI TU PATH DEL ARCHIVO sEAT.TXT";
-            
+            //String pathR1 = "PON AQUI TU PATH DEL ARCHIVO RENAULT.TXT";
+            //String pathR2 = "PON AQUI TU PATH DEL ARCHIVO sEAT.TXT";
+            BufferedReader reader = new BufferedReader(new FileReader(pathM1));
             renault = new File(pathM1);
             
-            if (!renault.exists()) {
-                System.out.println("el archivo no existe");
-            }
-            //Leemos archivo RENAULT
-            String linea;
+            String linea = null;
             fr = new FileReader(renault);
             br = new BufferedReader(fr);
-            while ((linea = br.readLine()) != null) {
-                arrayRenault.add(linea);
+            int x = 0;
+            Coche[] arrayCoches = new Coche[3];
+            String[] car = new String[10];
+            
+            
+            while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            // Split line on comma.
+            String[] parts = line.split(",");
+            for (String part : parts) {
+                //System.out.println(part);
+                arrayPalabras.add(part);
                 
             }
-            for(int i = 0; i < arrayRenault.size(); i++){
-                System.out.println(arrayRenault.get(i));
-            }
-            String paquete = "";
-            String paquete2 = "";
-/*
-            //System.out.print("EL VECTOR ES : \n ");//MOSTRAR LOS DATOS DEL ARREGLO
-            for (int j = 0; j < arrayRenault.size(); j++) {
-                paquete = arrayRenault.get(j);
-                linea = paquete.substring(0, 6);
-                if (linea.equals(args[0])) {
-                    System.out.print(paquete + "\n");
-                    paquete2 = paquete;
-                }
-            }
-*/
-
-
-            /*String[] pakage = paquete2.split(";");
-
-            String[] pakagesec = pakage[1].split(",");
-            if (pakagesec.length == 0) {
-                System.out.println("el ID no existe");
-            }
-            
-            //obtenemos el primer renault*****************************
-            archivo2 = new File("C:\\archivo1.txt");
-            if (!archivo2.exists()) {
-                System.out.println("el archivo no existe");
-            }*/
-            /*
-            fr = new FileReader(archivo2);//aqui lo leemos y lo pasamos a FileReader
-            br = new BufferedReader(fr);
-            String linea2;
-            while ((linea2 = br.readLine()) != null) {
-                arraySeat.add(linea2);
-            }*/
-            //obtenemos el segundo renault ***************************
-           /* for (int c = 0; c < pakagesec.length; c++) {
-
-                String[] pakage2 = null;
-                for (int j = 0; j < arraySeat.size(); j++) {
-                    pakage2 = arraySeat.get(j).split(";");
-                    if (pakage2[0].equals(pakagesec[c])) {
-                        linea2 = arraySeat.get(j);
-                    }
-                }
-                if (linea == null) {
-                    System.out.println("el paquete no existe");
-                }
-                pakage2 = linea2.split(";");
-//************************************** escritura*******************************/
-                /*FileWriter fw = new FileWriter("emailMainteiner" + c + " .txt");
-
-                String strs[] = {"From: correo@gmail.com", "To: " + pakage2[2], "Dear: " + pakage2[1], "You have a new bug: ", pakage2[0] + " RC bug number #" + pakage[0], "Please, fix it as soon as possible.", "Cheers."};
-                fw.flush();
-                for (int i = 0; i < strs.length; i++) {
-                    fw.write(strs[i] + "\n");
-                }
-                fw.close();*/
-          //  }
-
-        }//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY//FIN DEL TRY
+     
+        }
+        //System.out.println(arrayPalabras.get(5));
+        reader.close();
+        
+        mostrarModelo(arrayPalabras);
+        mostrarTodo(arrayPalabras, linea, br, car, arrayCoches, x);
+        mostrarPrecio(arrayPalabras);
+        }
         catch (Exception e) {
             System.out.println(e);
         }
         System.exit(0);
     }
-    
+    // Método para imprimir el array de Personas
+    static void imprimeArrayPersonas(Coche[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println((i+1) + ". " + array[i].getModelo() + " - Altura: " + array[i].getAño() + " - Edad: " + array[i].getKm() + " - Edad: " + array[i].getPrecio());
+        }
+    }
+    static void mostrarPrecio(ArrayList<String> arrayPalabras){
+        int max = 0;
+        String z = "";
+        int x = 0;
+        for (int i = 3; i < arrayPalabras.size(); i = i + 4) {
+            if (Integer.parseInt(arrayPalabras.get(i)) > max) {
+                max = Integer.parseInt(arrayPalabras.get(i));
+                z = arrayPalabras.get(x);
+                x = x + 4;
+            }
+        }
+        System.out.println("El coche más caro es: " + z + " con el precio " + max);
+        /*
+        int i = 3;
+        while(i < arrayPalabras.size()){
+                
+            System.out.println("Precio: " + arrayPalabras.get(i));
+                i = i + 4;
+            }*/
+    }
+    static void mostrarModelo(ArrayList<String> arrayPalabras){
+        int i = 0;
+        while(i < arrayPalabras.size()){
+                
+            System.out.println("Modelo: " + arrayPalabras.get(i));
+                i = i + 4;
+            }
+    }
+    static void mostrarTodo(ArrayList<String> arrayPalabras, String linea, BufferedReader br, String[] car, Coche[] arrayPersonas, int x) throws IOException{
+         while ((linea = br.readLine()) != null) {
+                arrayPalabras.add(linea);
+                car = linea.split(",");
+                //System.out.println(car[x]);
+                coche = new Coche(car[0], Integer.parseInt(car[1]), Integer.parseInt(car[2]), Double.parseDouble(car[3]));
+                System.out.println(coche);
+                arrayPersonas[x] = new Coche(car[0], Integer.parseInt(car[1]), Integer.parseInt(car[2]), Double.parseDouble(car[3]));
+                x++;
+            }
+    }
 }
